12ec4772f61531ef010397dd9d6d7c6fe8f64008
Maty Barrientos <r.m.barrientos@live.com>
2025-08-19 08:51:43 -0300

    TP1: ejercicios (completar el nro 5)

--- src/EJERCICIO1/Empleado.java
+++ src/EJERCICIO1/Empleado.java
@@ -1,29 +1,53 @@
 package EJERCICIO1;
 
-public class Empleado extends Persona{
-
+public class Empleado extends Persona {
+	private int legajo;
+	String Puesto;
+	private static int cont=1000;
 	
-	private static int legajo = 1000;
-	private String puesto;
-	
+	//constructor
 	public Empleado() {
-		legajo++;
-		this.puesto="a definir";
+	    super();
+	    cont++;
+	    this.legajo = cont;
 	}
-	
-	@Override
-	public String toString() {
-		
-		return super.toString() + " Empleado legajo=" + legajo + ", puesto=" + puesto;
+
+	public Empleado(String dni , String nombre , String apellido , java.time.LocalDate fechadenacimiento , 
+	 		String genero , String email , String direccion , String telefono , String puesto) {
+	    super(dni, nombre, apellido, fechadenacimiento, genero, email, direccion, telefono);
+	    cont++;
+	    this.legajo = cont;
+	    this.Puesto = puesto;
+	}
+
+	//metodos
+	public static int devuelveProximoLegajo() {
+		return cont++;
+	}
+
+	//GETTER Y SETTER
+	public int getLegajo() {
+		return legajo;
+	}
+
+	public void setLegajo(int legajo) {
+		this.legajo = legajo;
 	}
 
 	public String getPuesto() {
-		return puesto;
+		return Puesto;
 	}
 
 	public void setPuesto(String puesto) {
-		this.puesto = puesto;
+		Puesto = puesto;
 	}
+
+	// metodo toString
+	@Override
+	public String toString() {
+		return "Empleado [legajo=" + legajo + ", Puesto=" + Puesto + "]";
+	}
+	
 	
 	
 }
